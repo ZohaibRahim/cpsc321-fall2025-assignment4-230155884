e759edbe4c808192cac53c8fb85acf23a6ab7a4a
Zohaib Rahim <zrahim@unbc.ca>
2025-11-12 16:21:22 -0800

    Update safe state output and formatting in Assignment4.c

--- Assignment4.c
+++ Assignment4.c
@@ -4,42 +4,85 @@ int main()
 { 
 	// P0, P1, P2, P3, P4 are the names of Process
 
-	int cust, resource, i, j, k; 
-	cust = 5; // Indicates the Number of processes/customers 
-	resource = 4; //Indicates the Number of resources 
 
-	int avail[resource] = { 2, 3, 2 }; // the available amount of each resource
+
+	int cust, resource, i, j, k; 
+
+	//Customer/Processes
+	printf("Enter the number of customers (processes): ");
+	scanf("%d", &cust); //Indicates the Number of customers
+
+	//Resources
+	printf("Enter the number of resources: " );
+	scanf("%d", &resource); //Indicates the Number of resources
 	
-	int max[cust][resource] = { { 7, 6, 3 }, // P0 // the maximum demand of each customer
-					{ 3, 2, 2 }, // P1 
-					{ 8, 0, 2 }, // P2 
-					{ 2, 1, 2 }, // P3 
-					{ 5, 2, 3 } }; // P4 
+	//Number of instances of each resource type
+	int instances[resource]; //the total amount of each resource
+	printf("Enter number of instances of each resource (%d values): ", resource);
+	for (i = 0; i < resource; i++) {
+		scanf("%d", &instances[i]);
+	}
 	
-	int alloc[cust][resource] = { { 0, 0, 1 }, // P0 // the amount currently allocated to each customer  
-						{ 3, 0, 0 }, // P1 
-						{ 1, 0, 1 }, // P2 
-						{ 2, 3, 2 }, // P3 
-						{ 0, 0, 3 } }; // P4 
+	//Available Resources
+	int avail[resource]; //the available amount of each resource
+	printf("Enter avaialable resources (%d values): ", resource);
+	for (i = 0; i < resource; i++) {
+		scanf("%d", &avail[i]);
+	}
+
+	//Maximum Demand Matrix
+	int max[cust][resource]; //the maximum demand of each customer
+	printf("Enter maximum demand matrix(%d x %d):\n", cust, resource);
+	for (i = 0; i < cust; i++) {
+		printf("Customer %d: ", i);
+		for (j = 0; j < resource; j++) {
+			scanf("%d", &max[i][j]);
+		}
+	}
 	
-	//the remaining need of each customer
-	int need[cust][resource]; 
-	for (i = 0; i < n; i++) { 
-		for (j = 0; j < r; j++) 
+	//Allocation Matrix
+	int alloc[cust][resource]; // the amount currently allocated to each customer  
+	printf("Enter current allocation matrix(%d x %d):\n", cust, resource);
+	for (i = 0; i < cust; i++) {
+		printf("Customer %d: ", i);
+		for (j = 0; j < resource; j++) {
+			scanf("%d", &alloc[i][j]);
+		}
+	}
+	
+
+	//Need Matrix
+	int need[cust][resource]; //the remaining need of each customer
+	for (i = 0; i < cust; i++) { 
+		for (j = 0; j < resource; j++) 
 			need[i][j] = max[i][j] - alloc[i][j]; 
 	} 
-	
 
+	//Resource Request Algorithm
+	int request[resource]; //Request array
+	int req_cust; //Customer making the request
+	printf("\n\nEnter Resource Request (e.g., for Customer1 enter 1 and then requests 1 0 2): ");
+	scanf("%d", &req_cust);
+	for (i = 0; i < resource; i++) {
+		scanf("%d", &request[i]);
+	}
+
+	
+	
+	//Mark all customers as not finished
 	int f[cust], ans[cust], ind = 0; 
-	for (k = 0; k < n; k++) { 
+	for (k = 0; k < cust; k++) { 
 		f[k] = 0; 
 	} 
+
 	
+	
+	//Find the safe sequence
 	int y = 0; 
-	for (k = 0; k < 5; k++) { 
+	for (k = 0; k < cust; k++) { 
 		for (i = 0; i < cust; i++) { 
 			if (f[i] == 0) { 
-
+				//Check if need is less than available
 				int flag = 0; 
 				for (j = 0; j < resource; j++) { 
 					if (need[i][j] > avail[j]){ 
@@ -48,6 +91,7 @@ int main()
 					} 
 				} 
 
+				//If need is less than available, add to safe sequence
 				if (flag == 0) { 
 					ans[ind++] = i; 
 					for (y = 0; y < resource; y++) 
@@ -58,10 +102,13 @@ int main()
 		} 
 	} 
 
-	printf("Th SAFE Sequence is as follows\n"); 
+	//If safe sequence is found, print it
+	printf("The SAFE Sequence is as follows\n"); 
 	for (i = 0; i < cust - 1; i++) 
-		printf(" P%d ->", ans[i]); 
-	printf(" P%d", ans[cust - 1]); 
+		printf(" C%d ->", ans[i]); 
+	printf(" C%d", ans[cust - 1]); 
+
+	
 
 	return (0); 
 
